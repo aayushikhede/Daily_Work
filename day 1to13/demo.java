9bb0d5ea564c455acc4eaf61009758e32c4d9cbf
aayushikhede <42095664+aayushikhede@users.noreply.github.com>
2020-02-17 12:27:01 -0800

    something

---
+++ day 1to13/demo.java
@@ -0,0 +1,6 @@
+class q{
+public static void main(String args[])
+{
+System.out.println("s");
+}
+}
---
+++ day 1to13/pattern.java
@@ -0,0 +1,20 @@
+
+public class PatternExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i,j;
+       for(i=0;i<5;i++){
+    	   for(j=0;j<=i;j++)
+    	   {
+    		   System.out.println("* ");
+    	   }
+          System.out.println();
+       }
+       
+	}
+
+}
