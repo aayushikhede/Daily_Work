e1e47cc8162e486103dc9cbce2bec169103a3dea
aayushikhede <42095664+aayushikhede@users.noreply.github.com>
2020-02-18 04:14:47 -0800

    Add files via upload

---
+++ day15/stringclass/StringBuilderDemo.java
@@ -0,0 +1,26 @@
+package com.cts.training.stringclass;
+
+public class StringBuilderDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		StringBuilder sb= new StringBuilder();
+		System.out.println(sb.capacity());
+		sb.append("hello to java version 8");
+         System.out.println(sb.capacity());
+         sb.append("this is question for batvch 1");
+         System.out.println(sb.capacity());
+         sb.insert(7, " and welcome");
+         System.out.println(sb);
+         System.out.println(sb.capacity());
+         sb.replace(7, 15, "correct value is written now");
+         System.out.println(sb);
+         sb.delete(7,20);
+         System.out.println(sb);
+         sb.reverse();
+         System.out.println(sb);
+
+	}
+
+}
